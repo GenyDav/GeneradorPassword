850d38f91aa04a2b9735e7de96d0cbee0b5b1c65
GD <geny.d.mtz@hotmail.com>
2022-08-02 19:24:11 -0500

    Inicio de documentación

--- src/generadorcontraseña/NivelSeguridad.java
+++ src/generadorcontraseña/NivelSeguridad.java
@@ -1,34 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package generadorcontraseña;
 
 /**
- *
+ * Clase que realiza los cálculos para determinar que tan segura es la contraseña
+ * creada.
  * @author Geny
+ * @version 1.0
  */
 public class NivelSeguridad {
-    private int longitudPasswd;
-    private int totalCaracteres;
+    private int longitudPasswd;     // número de caracteres que tiene la contraseña
+    private int totalCaracteres;    // suma del número de caracteres de los grupos 
     
-    public NivelSeguridad(int tam, boolean mayus, boolean minus, boolean nums, boolean sims){
+    /**
+     * Constructor. Encargado de definir los valores utilizados para realizar el
+     * cálculo de la cantidad de tiempo necesaria para 'romper' una contraseña que
+     * tenga las características de los parámetros.
+     * @param tam Longitud de la contraseña
+     * @param minus Indica si la contraseña contiene o no letras minúsculas
+     * @param mayus Indica si la contraseña contiene o no letras mayúsculas
+     * @param nums Indica si la contraseña contiene o no números
+     * @param sims Indica si la contraseña contiene o no símbolos
+     */
+    public NivelSeguridad(int tam, boolean minus, boolean mayus, boolean nums, boolean sims){
         longitudPasswd = tam;
         totalCaracteres = calcularTotalCaracteres(mayus, minus, nums, sims);
     }
     
+    /**
+     * Método que realiza la suma del número de caracteres contenidos en cada
+     * grupo marcado por el usuario para obtener el valor N, que representa el 
+     * total de caracteres que pueden ser utilizados para crear una contraseña.
+     * @param mayus Indica si se debe sumar la cantidad de letras mayúsculas (26) al total de caracteres
+     * @param minus Indica si se debe sumar la cantidad de letras minúsculas (26) al total de caracteres
+     * @param nums Indica si se debe sumar la cantidad de números (10) al total de caracteres
+     * @param sims Indica si se debe sumar la cantidad de símbolos (10) al total de caracteres
+     * @return Cantidad de caracteres que pueden utilizarse al crear una contraseña
+     */
     private int calcularTotalCaracteres(boolean mayus, boolean minus, boolean nums, boolean sims){
         int total = 0;
         if(mayus) total += 26;
         if(minus) total += 26;
         if(nums) total += 10;
         if(sims) total += 10;
-        System.out.println("N: " + total);
-        System.out.println("L: " + longitudPasswd);
         return total;
     }
     
+    /**
+     * Método que realiza el cálculo del tiempo (en años) requerido para romper 
+     * una contraseña. Primero se calcula el número de contraseñas posibles de 
+     * longitud L que se pueden crear utilizando un conjunto caracteres de tamaño 
+     * N efectuando la operación N^L. Despues, la cantidad obtenida se divide entre
+     * el resultado de multiplicar 40x10^9 (número de hashes por segundo de una
+     * tarjeta gráfica RTX 2080) por 31,556,926 (cantidad de segundos en un año).
+     * Información obtenida de https://www.hivesystems.io/blog/are-your-passwords-in-the-green
+     * @return 
+     */
     public double calcularTiempoProcesamiento(){
         double totalCombinaciones = Math.pow(totalCaracteres,longitudPasswd);
         System.out.println("Combinaciones: "+ totalCombinaciones);
